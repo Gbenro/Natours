5013f52366f9adad283237e16efcd42195f2f3da
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2021-12-28 08:14:49 -0600

    added filters to queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,24 @@ const Tour = require('./../models/tourModels');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Build query
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
 
+    console.log(req.query, queryObj);
+
+    const query = await Tour.find(queryObj);
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //Execute Query
+    const tours = await query;
+
+    //Send Response
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -79,13 +95,12 @@ exports.updateTour = async (req, res) => {
 };
 
 exports.deleteTour = async (req, res) => {
-  try{
-
-    await Tour.findByIdAndDelete(req.params.id)
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
