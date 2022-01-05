ef5ad24f74637719a559d3ba1105617f4cb2d997
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-05 07:24:35 -0600

    added 404 errors

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,10 +1,42 @@
+const sendErrorDev = (err, res) => {
+  //Operational trusted error,:send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+    //Programming or other error: we dont want to leak error to client
+  } else {
+    //1) log the error
+    console.error('ERROR', err);
+    //2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 
   next();
 };
