b28c155f8b271040c60e35297f5be877e2d2af4b
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-05 06:19:32 -0600

    better error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
@@ -24,29 +26,21 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `can't find original ${req.originalUrl} on this server `,
-  // });
-
-  const err = new Error(
-    `can't find original ${req.originalUrl} on this server `
+  next(
+    new AppError(`can't find original ${req.originalUrl} on this server `, 404)
   );
-  err.status = fail;
-  err.statusCode = 404;
-
-  next(err);
 });
+app.use(globalErrorHandler);
+// app.use((err, req, res, next) => {
+//   console.log(err.stack);
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+//   });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-
-  next();
-});
+//   next();
+// });
 
 module.exports = app;
