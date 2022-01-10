5386c12fe208d4015151fa599bc28c9443092bfa
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-10 07:39:12 -0600

    loggin in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
+const User = require('../models/userModels');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'err',
-    message: 'route not yet defined',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+  next();
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'err',
