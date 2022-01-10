cb9296ffe1d2986be18f3d2e4b7ed738e763bc56
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-10 06:25:18 -0600

    hashing password

--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -19,13 +20,30 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8
+    minlength: 8,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
-    minlength: 8
+    minlength: 8,
+    validate: {
+      //This only works on save!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  //Only run this function iff password is modified
+  if (!this.isModified('password')) return next();
+  //hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+  //delete the passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
