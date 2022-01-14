c06ab3f84955e7b71d64fabc8ed59c250f520b0f
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-14 06:06:18 -0600

    authorization: user roles and permission

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const authController = require('./../controllers/authController')
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
-
-
 
 //router.param('id', tourController.checkID);
 
@@ -16,12 +14,16 @@ router.route('/tour-stats').get(tourController.getToursStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
-  .get(authController.protect,tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;
