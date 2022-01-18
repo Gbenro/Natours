6cc3889f0f17407464811d490118b689be8b3664
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-18 07:23:09 -0600

    preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
@@ -35,8 +36,24 @@ app.use(express.json({ limit: '10kb' }));
 
 //Data sanitiztion against Nosql query injection
 app.use(mongoSanitize());
+
 //Data snaitization against XSS
 app.use(xss());
+
+//Prevent parameter solution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 //serving static file
 app.use(express.static(`${__dirname}/public`));
 
